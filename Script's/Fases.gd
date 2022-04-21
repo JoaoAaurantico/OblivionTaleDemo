ae5d9e7d0177e8e3e4800d93a3841ed976c86441
VirusAurantico <virus15dzn@hotmail.com>
2022-04-21 18:16:56 -0300

    Alteração fase01
    Alteração de script de pulo e alteração da fase01

--- Script's/Fases.gd
+++ Script's/Fases.gd
@@ -5,3 +5,7 @@ func _ready():
 	if get_tree().current_scene.name == "Fase01":
 		if Global.checkpoint != Vector2(0,0):
 			$Player.global_position = Global.checkpoint
+
+func _on_Area2D_area_entered(area):
+	if get_tree().current_scene.name == "Fase01":
+		$AnimationPlayer.play("Mapa")
