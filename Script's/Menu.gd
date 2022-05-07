7e2d0b5956c4d230ca0664df705ccaa6b7a207cd
VirusAurantico <virus15dzn@hotmail.com>
2022-05-07 13:38:04 -0300

    Creditos
    Agora tem creditos

--- Script's/Menu.gd
+++ Script's/Menu.gd
@@ -2,16 +2,27 @@ extends CanvasLayer
 
 var entrou = false
 
+func _process(_delta):
+	if $Creditos.visible == true:
+		$Idioma.text = "<--"
+	if Global.ingles == true && !$Creditos.visible == true:
+		$Idioma.text = "EN-US"
+	elif Global.ingles == false && !$Creditos.visible == true:
+		$Idioma.text = "PT-BR"
+
+
 func _ready():
 	if Global.ingles == true:
 		$Idioma.text = "EN-US"
 		$Pause/HBoxContainer/CenterContainer/VBoxContainer/Play.text = "Play"
 		$Pause/HBoxContainer/CenterContainer/VBoxContainer/Continue.text = "Continue"
+		$Pause/HBoxContainer/CenterContainer/VBoxContainer/Credits.text = "Credits"
 		$Pause/HBoxContainer/CenterContainer/VBoxContainer/Quit.text = "Quit"
 	else:
 		$Idioma.text = "PT-BR"
 		$Pause/HBoxContainer/CenterContainer/VBoxContainer/Play.text = "Iniciar"
 		$Pause/HBoxContainer/CenterContainer/VBoxContainer/Continue.text = "Continuar"
+		$Pause/HBoxContainer/CenterContainer/VBoxContainer/Credits.text = "Creditos"
 		$Pause/HBoxContainer/CenterContainer/VBoxContainer/Quit.text = "Sair"
 
 func _on_Play_pressed():
@@ -38,19 +49,26 @@ func _on_Continue_pressed():
 
 func _on_Idioma_pressed():
 	AudioGeral.emit_signal("iniciar", "click")
-	if Global.ingles == true:
+	if $Idioma.text == "<--":
+		$Creditos.visible =  false
+	if Global.ingles == true && !$Idioma.text == "<--":
 		Global.ingles = false
 		$Idioma.text = "PT-BR"
 		$Pause/HBoxContainer/CenterContainer/VBoxContainer/Play.text = "Iniciar"
 		$Pause/HBoxContainer/CenterContainer/VBoxContainer/Continue.text = "Continuar"
+		$Pause/HBoxContainer/CenterContainer/VBoxContainer/Credits.text = "Creditos"
 		$Pause/HBoxContainer/CenterContainer/VBoxContainer/Quit.text = "Sair"
-	else:
+	elif Global.ingles == false && !$Idioma.text == "<--":
 		Global.ingles = true
 		$Idioma.text = "EN-US"
 		$Pause/HBoxContainer/CenterContainer/VBoxContainer/Play.text = "Play"
 		$Pause/HBoxContainer/CenterContainer/VBoxContainer/Continue.text = "Continue"
+		$Pause/HBoxContainer/CenterContainer/VBoxContainer/Credits.text = "Credits"
 		$Pause/HBoxContainer/CenterContainer/VBoxContainer/Quit.text = "Quit"
 
 
 func _on_mouse_entered():
 	AudioGeral.emit_signal("iniciar", "hover")
+
+func _on_Credits_pressed():
+	$Creditos.visible = true
