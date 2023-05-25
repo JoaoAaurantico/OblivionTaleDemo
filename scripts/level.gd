c503f00004ed149e6660a083289a8e1722c3a5cd
VirusAurantico <86685027+VirusAurantico@users.noreply.github.com>
2023-05-25 16:34:15 -0300

    Fase01 Only

--- scripts/level.gd
+++ scripts/level.gd
@@ -4,8 +4,3 @@ func _ready():
 	if get_tree().current_scene.name == "level":
 		if Global.checkpoint != Vector2(0,0):
 				$player.global_position = Global.checkpoint
-
-
-func _on_Area2D_area_entered(_area):
-	$Boss/AnimationPlayer.play("Boss")
-	$Boss/bozinarea.queue_free()
